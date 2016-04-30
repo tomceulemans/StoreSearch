944111f28dc7121983bd04779deef84b423788ef
Tom Ceulemans <tomceulemans@users.noreply.github.com>
2016-04-30 22:12:20 +0200

    Display images in search results

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class SearchResultCell: UITableViewCell {
+    var downloadTask: NSURLSessionDownloadTask?
+    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImage: UIImageView!
-    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,5 +27,44 @@ class SearchResultCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        downloadTask?.cancel()
+        downloadTask = nil
+        
+        nameLabel.text = ""
+        artistNameLabel.text = ""
+        artworkImage.image = nil
+    }
+    
+    func configureForSearchResult(searchResult: SearchResult) {
+        nameLabel.text = searchResult.name
+        
+        if (searchResult.artistName.isEmpty) {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", searchResult.artistName, kindForDisplay(searchResult.kind))
+        }
+        
+        artworkImage.image = UIImage(named: "Placeholder")
+        if let url = NSURL(string: searchResult.artworkURL60) {
+            downloadTask = artworkImage.loadImageWithURL(url)
+        }
+    }
 
+    func kindForDisplay(kind: String) -> String {
+        switch kind {
+        case "album": return "Album"
+        case "audiobook": return "Audio Book"
+        case "book": return "Book"
+        case "ebook": return "E-Book"
+        case "feature-movie": return "Movie" case "music-video": return "Music Video" case "podcast": return "Podcast"
+        case "software": return "App"
+        case "song": return "Song"
+        case "tv-episode": return "TV Episode"
+        default: return kind
+        }
+    }
 }
